94413a053613d019511d26591db9b2fb3683128f
Kevin Trieu <dragn194@gmail.com>
2016-09-17 03:26:57 -0400

    Convert sparks-web to handle emergencies

--- sparks-web.go
+++ sparks-web.go
@@ -73,42 +73,26 @@ func main() {
     })
     
     // resume
-    router.Get("/viewResumeJSON/:resumeID", viewResumeJSON)
+    router.Get("/viewEmergencyJSON/:emergencyId", viewEmergencyJSON)
     
     // login & logout
     router.Post("/checkLoginInfoJSON", checkLoginInfoJSON)
     router.Get("/logoutJSON", logoutJSON)
-    
-    // dashboard
-    router.Get("/loadDashboardResumesJSON", loadDashboardResumesJSON)
-    router.Get("/loadDashboardPortfoliosJSON", loadDashboardPortfoliosJSON)
-    router.Get("/loadSettingsJSON", loadSettingsJSON)
-    
+       
     // resume
-    router.Get("/loadResumeJSON/:resumeID", loadResumeJSON)
-    router.Post("/insertResumeJSON/:category", insertResumeJSON)
-    router.Post("/updateResumeJSON/:category/:resumeID", updateResumeJSON)
-    router.Delete("/deleteResumeJSON/:resumeID", deleteResumeJSON)
-    
-    // portfolio
-    router.Get("/loadPortfolioJSON/:portfolioID", loadPortfolioJSON)
-    router.Post("/insertPortfolioJSON/:category", insertPortfolioJSON)
-    router.Post("/updatePortfolioJSON/:category/:portfolioID", updatePortfolioJSON)
-    router.Delete("/deletePortfolioJSON/:portfolioID", deletePortfolioJSON)
-    
+    router.Get("/loadEmergencyJSON/:emergencyId", loadEmergencyJSON)
+    router.Post("/insertEmergencyJSON/:emergencyId", insertEmergencyJSON)
+    router.Post("/updateEmergencyJSON/:emergencyId", updateEmergencyJSON)
+    router.Delete("/deleteEmergencyId/:emergencyId", deleteEmergencyId)
+ 
     // user
     router.Post("/insertUserJSON", insertUserJSON)
     router.Post("/updateUserJSON", updateUserJSON)
     router.Delete("/deleteUserJSON", deleteUserJSON)
     
     // view
-    router.Get("/user/resume/-1", viewUser)
-    router.Get("/user/resume/:resumeID", viewUser)
-    router.Get("/user/resumes", viewUser)
-    router.Get("/user/portfolios", viewUser)
-    router.Get("/user/settings", viewUser)
     router.Get("/login", viewLogin)
-    router.Get("/resume/:resumeID", viewResume)
+    router.Get("/emergency/:emergencyId", viewEmergency)
     router.Get("/", viewIndex)
    
     log.Println("Listening...")
@@ -149,7 +133,7 @@ func viewIndex(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func viewResume(w http.ResponseWriter, r *http.Request) {
+func viewEmergency(w http.ResponseWriter, r *http.Request) {
     returnCode := 0
 
     setHeader(w)
@@ -237,27 +221,27 @@ func viewUser(w http.ResponseWriter, r *http.Request) {
   ========================================
 */
 
-func viewResumeJSON(w http.ResponseWriter, r *http.Request) {
+func viewEmergencyJSON(w http.ResponseWriter, r *http.Request) {
     returnCode := 0
     
-    resume := new(Resume)    
-    resumeID := vestigo.Param(r, "resumeID")
+    emergency := new(Emergency)    
+    id := vestigo.Param(r, "emergencyId")
 
-    selector := bson.M{"resumeid": resumeID}
+    selector := bson.M{"id": id}
 
-    if err := loadResumeDB(resume, &selector); err != nil {
+    if err := loadEmergencyDB(emergency, id); err != nil {
         returnCode = 1
     }
 
     if returnCode == 0 {
-        if err := json.NewEncoder(w).Encode(resume); err != nil {
+        if err := json.NewEncoder(w).Encode(emergency); err != nil {
             returnCode = 2
         }
     }
 
     // error handling
     if returnCode != 0 {
-        handleError(returnCode, errorStatusCode, "Resume could not be loaded at this time.", w)
+        handleError(returnCode, errorStatusCode, "Emergency could not be loaded at this time.", w)
     }
 }
 
@@ -353,416 +337,108 @@ func deleteSession(w http.ResponseWriter, r *http.Request) error {
 
 /*
   ========================================
-  Dashboard
-  ========================================
-*/
-
-func loadDashboardResumesJSON(w http.ResponseWriter, r *http.Request) {
-    returnCode := 0
-    
-    if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        user := new(User)
-        user.UserID = uID.(string)
-
-        if err = loadDashboardResumesDB(user); err != nil {
-            returnCode = 1
-        }
-
-        if returnCode == 0 {
-            if err = json.NewEncoder(w).Encode(user); err != nil {
-                returnCode = 2
-            }
-        }
-        
-        // error handling
-        if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Dashboard resumes could not be loaded at this time.", w)
-        }
-    } else {
-        handleError(3, 403, "Session expired. Please sign in again.", w)
-    }
-}
-
-func loadDashboardPortfoliosJSON(w http.ResponseWriter, r *http.Request) {
-    returnCode := 0
-
-    if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        user := new(User)
-        user.UserID = uID.(string)
-
-        if err = loadDashboardPortfoliosDB(user); err != nil {
-            returnCode = 1
-        }
-
-        if returnCode == 0 {
-            if err = json.NewEncoder(w).Encode(user); err != nil {
-                returnCode = 2
-            }
-        }
-        
-        // error handling
-        if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Dashboard portfolios could not be loaded at this time.", w)
-        }
-    } else {
-        handleError(3, 403, "Session expired. Please sign in again.", w)
-    }
-}
-
-func loadSettingsJSON(w http.ResponseWriter, r *http.Request) {
-    returnCode := 0
-    
-    if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        user := new(User)
-        user.UserID = uID.(string)
-
-        if err = loadSettingsDB(user); err != nil {
-            returnCode = 1
-        }
-
-        if returnCode == 0 {
-            if err = json.NewEncoder(w).Encode(user); err != nil {
-                returnCode = 2
-            }
-        }
-
-        // error handling
-        if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Settings could not be loaded at this time.", w)
-        }
-    } else {
-        handleError(3, 403, "Session expired. Please sign in again.", w)
-    }
-}
-
-/*
-  ========================================
   Resume
   ========================================
 */
 
-func loadResumeJSON(w http.ResponseWriter, r *http.Request) {
+func loadEmergencyJSON(w http.ResponseWriter, r *http.Request) {
     returnCode := 0
     
     if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        resume := new(Resume)    
-        resumeID := vestigo.Param(r, "resumeID")
-        userID := uID.(string)
+        emergency := new(Emergency)    
+        id := vestigo.Param(r, "emergencyId")
 
-        if resumeID != "-1" { // existing resume
-            selector := bson.M{"resumeid": resumeID, "userid": userID}
+        if err = loadEmergencyDB(emergency, id); err != nil {
+            returnCode = 1
+        }
 
-            if err = loadResumeDB(resume, &selector); err != nil {
+        if returnCode == 0 {
+            if err = json.NewEncoder(w).Encode(emergency); err != nil {
+                returnCode = 2
+            }
+        }
+
+        // error handling
+        if returnCode != 0 {
+            handleError(returnCode, errorStatusCode, "Emergency could not be loaded at this time.", w)
+        }
+    } else {
+        handleError(3, 403, "Session expired. Please sign in again.", w)
+    }
+}
+
+func insertEmergencyJSON(w http.ResponseWriter, r *http.Request) {
+    returnCode := 0
+    
+    if uID, err := readSession("userID", w, r); err == nil && uID != nil {
+        emergency := new(Emergency)
+		json.NewDecoder(r.Body).Decode(emergency)
+
+        if returnCode == 0 {
+            if emergency.Id, err = insertEmergencyDB(emergency); err != nil { // Step 1
                 returnCode = 1
             }
-        } else { // new resume
-            resume.ResumeID = "-1"
-            resume.Title = "Untitled Resume"
         }
 
         if returnCode == 0 {
-            if err = json.NewEncoder(w).Encode(resume); err != nil {
+            if err = json.NewEncoder(w).Encode(emergency); err != nil {
                 returnCode = 2
             }
         }
 
         // error handling
         if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Resume could not be loaded at this time.", w)
+            handleError(returnCode, errorStatusCode, "Emergency could not be inserted at this time.", w)
         }
     } else {
         handleError(3, 403, "Session expired. Please sign in again.", w)
     }
 }
 
-func insertResumeJSON(w http.ResponseWriter, r *http.Request) {
+func updateEmergencyJSON(w http.ResponseWriter, r *http.Request) {
     returnCode := 0
     
     if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        categoryInt := 0
-        resume := new(Resume)
-        resume.UserID = uID.(string)
-        user := new(User)
-        user.UserID = uID.(string)
-        // **** add first name and last name to resume.contact section ****
+        emergency := new(Emergency)
+		emergency.Id = vestigo.Param(rm "emergencyId")
+		json.NewDecoder(r.Body).Decode(emergency)
 
-        categoryStr := vestigo.Param(r, "category") // 1 = general | 2 = profile | 3 = experience | 4 = skills | 5 = portfolio | 6 = otherInfo | 7 = contactInfo
-
-        if categoryInt, err = strconv.Atoi(categoryStr); err != nil {
-            returnCode = 1
-        }
+		if err = updateEmergencyDB(emergency); err != nil {
+			returnCode = 1
+		}
 
         if returnCode == 0 {
-            if resume.ResumeID, err = insertResumeDB(resume.UserID); err != nil { // Step 1
+            if err = json.NewEncoder(w).Encode(emergency); err != nil {
                 returnCode = 2
-            }
-        }
-
-        if returnCode == 0 {
-            if err = insertUResume(resume.UserID, resume.ResumeID); err != nil { // Step 2
-                returnCode = 3
-            }
-        }
-
-        if returnCode == 0 {
-            if err = updateResume(categoryInt, resume, r); err != nil { // Step 3
-                returnCode = 4
-            }
-        }
-
-        if returnCode == 0 {
-            if err = loadSettingsDB(user); err != nil { // Step 4
-                returnCode = 5
-            }
-        }
-
-        if returnCode == 0 {
-            if err = updateRContactTypeName(user.UserID, user.FirstName, user.LastName); err != nil { // Step 5
-                returnCode = 6
-            }
-        }
-
-        if returnCode == 0 {
-            if err = json.NewEncoder(w).Encode(resume); err != nil {
-                returnCode = 7
             }
         }
 
         // error handling
         if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Resume could not be inserted at this time.", w)
+            handleError(returnCode, errorStatusCode, "Emergency could not be updated at this time.", w)
         }
     } else {
         handleError(3, 403, "Session expired. Please sign in again.", w)
     }
 }
 
-func updateResumeJSON(w http.ResponseWriter, r *http.Request) {
-    returnCode := 0
-    
-    if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        categoryInt := 0
-        resume := new(Resume)
-        resume.UserID = uID.(string)
-
-        categoryStr := vestigo.Param(r, "category")
-        /*
-        1 = settings
-
-        2 = profileType
-        3 = experienceType
-        4 = skillsType
-        5 = portfolioType
-        6 = otherInfoType
-        7 = contactInfoType
-
-        8 = experiences
-        9 = skills
-        */
-
-        if categoryInt, err = strconv.Atoi(categoryStr); err != nil {
-            returnCode = 1
-        }
-
-        if returnCode == 0 {
-            resume.ResumeID = vestigo.Param(r, "resumeID")
-
-            if err = updateResume(categoryInt, resume, r); err != nil {
-                returnCode = 2
-            }
-        }
-
-        if returnCode == 0 {
-            if err = json.NewEncoder(w).Encode(resume); err != nil {
-                returnCode = 3
-            }
-        }
-
-        // error handling
-        if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Resume could not be updated at this time.", w)
-        }
-    } else {
-        handleError(3, 403, "Session expired. Please sign in again.", w)
-    }
-}
-
-func updateResume(categoryInt int, resume *Resume, r *http.Request) error {
-    var err error
-
-    switch categoryInt {
-        case 1:
-            // part 1: update resume settings
-
-            err = json.NewDecoder(r.Body).Decode(resume)
-            logErrorMessage(err)
-
-            resume.TitleURL = getTitleURL(resume.Title)
-            resume.LinkURL = getLinkURL(resume.Title)
-
-            err = updateRSettings(resume.ResumeID, resume.UserID, resume)
-            logErrorMessage(err)
-
-            // part 2: update user resume
-
-            basicResume := new(BasicResume)
-
-            basicResume.ResumeID = resume.ResumeID
-            basicResume.Title = resume.Title
-            
-            err = updateUResume(resume.UserID, basicResume)
-            logErrorMessage(err)
-        case 2:
-            profile := new(ProfileType)
-
-            err = json.NewDecoder(r.Body).Decode(profile)
-            logErrorMessage(err)
-
-            // profile.Background = new(Image)
-
-            err = updateRProfileType(resume.ResumeID, resume.UserID, profile)
-            logErrorMessage(err)
-
-            resume.Profile = *profile
-        case 3:
-            experience := new(ExperienceType)
-            
-            err = json.NewDecoder(r.Body).Decode(experience)
-            logErrorMessage(err)
-            
-            // experience.Background = new(Image)
-            
-            err = updateRExperienceType(resume.ResumeID, resume.UserID, experience)
-            logErrorMessage(err)
-            
-            resume.Experience = *experience
-        case 4:
-            skills := new(SkillsType)
-            
-            err = json.NewDecoder(r.Body).Decode(skills)
-            logErrorMessage(err)
-            
-            // skills.Background = new(Image)
-
-            err = updateRSkillsType(resume.ResumeID, resume.UserID, skills)
-            logErrorMessage(err)
-
-            resume.Skills = *skills
-        case 5:
-            updateRPortfolioType()
-        case 6:
-            updateROtherInfoType()
-        case 7:
-            contact := new(ContactType)
-
-            err = json.NewDecoder(r.Body).Decode(contact)
-            logErrorMessage(err)
-
-            // contactInfo.Background = new(Image)
-
-            err = updateRContactType(resume.ResumeID, resume.UserID, contact)
-            logErrorMessage(err)
-
-            resume.Contact = *contact
-        case 8:
-            var experiences []Experience
-            var byteSlice []byte
-            
-            // https://golang.org/pkg/io/ioutil/#ReadAll
-            byteSlice, err = ioutil.ReadAll(r.Body)
-            logErrorMessage(err)
-            
-            // https://golang.org/pkg/encoding/json/#Unmarshal
-            err = json.Unmarshal(byteSlice, &experiences)
-            logErrorMessage(err)
-            
-            err = updateRExperiences(resume.ResumeID, resume.UserID, &experiences)
-            logErrorMessage(err)
-            
-            resume.Experience.Experiences = experiences
-        case 9:
-            var skills []Skill
-            var byteSlice []byte
-
-            // https://golang.org/pkg/io/ioutil/#ReadAll
-            byteSlice, err = ioutil.ReadAll(r.Body)
-            logErrorMessage(err)
-            
-            // https://golang.org/pkg/encoding/json/#Unmarshal
-            err = json.Unmarshal(byteSlice, &skills)
-            logErrorMessage(err)
-            
-            err = updateRSkills(resume.ResumeID, resume.UserID, &skills)
-            logErrorMessage(err)
-           
-            resume.Skills.Skills = skills
-        default:
-            log.Println("No category selected. Resume not updated.")
-    }
-    
-    return err
-}
-
-func deleteResumeJSON(w http.ResponseWriter, r *http.Request) {
+func deleteEmergencyJSON(w http.ResponseWriter, r *http.Request) {
     returnCode := 0
 
     if uID, err := readSession("userID", w, r); err == nil && uID != nil {
-        userID := uID.(string)
-        resumeID := vestigo.Param(r, "resumeID")
+        id := vestigo.Param(r, "emergencyId")
 
-        if err := deleteUResume(userID, resumeID); err != nil { // part 1: delete user resume info
+        if err := deleteEmergencyDB(id); err != nil {
             returnCode = 1
-        }
-
-        if returnCode == 0 {
-            if err := deleteResumeDB(resumeID, userID); err != nil { // part 2: delete resume
-                returnCode = 2
-            }
-        }
-
-        if returnCode == 0 {
-            if err := json.NewEncoder(w).Encode(""); err != nil {
-                returnCode = 3
-            }
         }
 
         // error handling
         if returnCode != 0 {
-            handleError(returnCode, errorStatusCode, "Resume could not be deleted at this time.", w)
+            handleError(returnCode, errorStatusCode, "Emergency could not be deleted at this time.", w)
         }
     } else {
         handleError(3, 403, "Session expired. Please sign in again.", w)
     }
-}
-
-/*
-  ========================================
-  Portfolio
-  ========================================
-*/
-
-func loadPortfolioJSON(w http.ResponseWriter, r *http.Request) {
-    
-    // **** EDIT ****
-    
-}
-
-func insertPortfolioJSON(w http.ResponseWriter, r *http.Request) {
-    
-    // **** EDIT ****
-    
-}
-
-func updatePortfolioJSON(w http.ResponseWriter, r *http.Request) {
-    
-    // **** EDIT ****
-    
-}
-
-func deletePortfolioJSON(w http.ResponseWriter, r *http.Request) {
-
-    // **** EDIT ****
-    
 }
 
 /*
